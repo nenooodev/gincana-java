6e92bfc775d7014d9d2625daee96a50717b05fa3
“mamarod428” <nonconformist3377@gmail.com>
2026-09-24 13:23:04 +0200

    enunciado reto 3

---
+++ gincana-java25/04-gincana-java25/src/Reto03.java
@@ -0,0 +1,11 @@
+//RETO 3 - Figuras que hablan
+//
+//Crea una clase abstracta Figura con area() y dos clases: Cuadrado y Circulo.
+//Usa lado 4 y radio 1. Calcula las dos areas mediante referencias de tipo Figura.
+//Determina cual esta mas cerca de 3.14. Imprime nombre de la ganadora y areas.
+//Ultima linea: CLAVE=R3-ganadora-areaCirculo-areaCuadrado
+//Ganadora en mayusculas, areas con dos decimales y punto (Locale.ROOT).
+
+
+public class Reto03 {
+}
